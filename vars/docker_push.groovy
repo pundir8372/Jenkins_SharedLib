20479e2a97030ae188c9836876e40a049e7321a3
pundir8372 <149288710+pundir8372@users.noreply.github.com>
2025-01-22 18:50:34 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
 def call(String projectName, String ImageTag) {
-    withCredentials([usernamePassword(credentialsId: 'dockerHubCreds', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
         // Log in to Docker Hub
         sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
     }
