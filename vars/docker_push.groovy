64a3d9215522197fc0a3cfd6b7f37d54ca39488a
pundir8372 <149288710+pundir8372@users.noreply.github.com>
2025-01-22 19:09:14 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,12 @@
-def call(String projectName, String ImageTag) {
+def call(String imageName, String imageTag) {
     withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
         // Log in to Docker Hub
         sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+        
+        // Tag the Docker image (ensure it's tagged correctly before pushing)
+        sh "docker tag ${imageName}:${imageTag} ${dockerHubUser}/${imageName}:${imageTag}"
+        
+        // Push the Docker image
+        sh "docker push ${dockerHubUser}/${imageName}:${imageTag}"
     }
-    // Push the Docker image
-    sh "docker push ${dockerHubUser}/${projectName}:${ImageTag}"
 }
